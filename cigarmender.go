4f055bc0df7364c8ff808ff14b55f00fab19f142
Steve Lancashire <stlancashire@gmail.com>
2026-06-22 14:08:56 +1000

    Added some time tracking to execution

--- cigarmender.go
+++ cigarmender.go
@@ -5,32 +5,40 @@ import (
 	"cigarmender/bamreader"
 	"log"
 
+	"time"
+
 	"github.com/biogo/hts/sam"
 )
 
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
 func main() {
+	defer timeTrack(time.Now(), "CIGARMender execution")
 
 	args := args.ParseArgs()
 
 	log.Printf("Processing cigarmender with %s", args.ToString())
 
-	delCounter := bamreader.DelCounter{}
-	count, err := bamreader.ReadBam(args.Input, &delCounter, args)
+	delCounter := getVisitor(args)
+	count, err := bamreader.ReadBam(args.Input, delCounter, args)
 	if err != nil {
 		log.Fatalf("Error reading bam %v", err)
 	}
 	log.Printf("Processed %d reads", count)
 
-	log.Printf(delCounter.Summary())
+	log.Println(delCounter.Summary())
 }
 
 func getVisitor(args args.Args) bamreader.ReadVisitor {
 	if args.DryRun {
 		return &bamreader.DelCounter{}
-	} else {
-		log.Fatalf("Implement me")
-		return nil
 	}
+
+	log.Fatalf("Implement me")
+	return nil
 }
 
 func VisitRead(r *sam.Record, val string) error {
